1d102a55654bae5c6eb00e171186fae99d93d2d4
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2022-03-09 02:14:12 +0000

    Addresses Peer-Review
    Removes Data Folder. It will be controlled by the config.json.
    Removes AlgorithmImports, since it is fetched with the nuget package and should not be edited.

--- DataProcessing/process.py
+++ DataProcessing/process.py
@@ -3,12 +3,13 @@
 # 3. Output data by date tp /temp-output-directory/alternative/quiver/twitter/universe/{date}.csv
 # 4. Output data for each ticker to /temp-output-directory/alternative/quiver/twitter/{symbol}.csv
 
-from clr_loader import get_coreclr
 import os
+from clr_loader import get_coreclr
 from pythonnet import set_runtime
 set_runtime(get_coreclr(os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")))
 
 from AlgorithmImports import *
+from QuantConnect.Lean.Engine.DataFeeds import *
 AddReference("Fasterflect")
 
 import pathlib
@@ -16,61 +17,80 @@ import requests
 import shutil
 import time
 
-file_path = '/temp-output-directory/alternative/quiver/twitter/'
-universe_file_path = file_path + 'universe/'
-if os.path.exists(universe_file_path):
-    shutil.rmtree(universe_file_path)
+VendorName = "quiver";
+VendorDataName = "twitter";
+
+class QuiverQuantTwitterFollowersDataDownloader:
+    def __init__(self, destinationFolder, canCreateUniverseFiles, apiKey = None):
+        self.destinationFolder = os.path.join(destinationFolder, VendorDataName)
+        self.universeFolder = os.path.join(self.destinationFolder, "universe")
+        self.canCreateUniverseFiles = canCreateUniverseFiles
+        self.clientKey = apiKey if apiKey else os.environ["QUIVER_API_KEY"]
+
+        if os.path.exists(self.universeFolder):
+            shutil.rmtree(self.universeFolder)
     
-pathlib.Path(file_path).mkdir(parents=True, exist_ok=True)
-pathlib.Path(universe_file_path).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(self.destinationFolder).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(self.universeFolder).mkdir(parents=True, exist_ok=True)
 
-headers = {
-    'accept': 'application/json',
-    'Authorization': 'Token ' + os.environ["QUIVER_API_KEY"]
-}
+    def Run(self):
 
-base_url = 'https://api.quiverquant.com/beta'
-companies_url = f'{base_url}/companies'
-twitter_url = f'{base_url}/historical/twitter'
+        mapFileProvider = LocalZipMapFileProvider()
+        mapFileProvider.Initialize(DefaultDataProvider());
 
-companies = requests.get(companies_url, headers=headers).json()
-companies = sorted(companies, key=lambda x: x['Ticker'])
+        companies = sorted(self.HttpRequester("companies"), key=lambda x: x['Ticker'])
+    
+        for c in companies:
+            ticker = c['Ticker']
+            filename = os.path.join(self.destinationFolder, f'{ticker.lower()}.csv')
+            print(f'Processing ticker: {ticker}')
+            i = 5
 
-for c in companies:
-    ticker = c['Ticker']
-    print(f'Processing ticker: {ticker}')
-    i = 5
+            while i != 0:
+                try:
+                    ticker_twitter = self.HttpRequester(f"historical/{VendorDataName}/{ticker}")
+                    time.sleep(0.03)
 
-    while i != 0:
-        try:
-            ticker_twitter = requests.get(f'{twitter_url}/{ticker}', headers=headers).json()
-            time.sleep(0.03)
+                    if len(ticker_twitter) == 0:
+                        print(f'No data for: {ticker}')
+                        break
 
-            if len(ticker_twitter) == 0:
-                print(f'No data for: {ticker}')
-                break
+                    lines = []
 
-            lines = []
+                    for row in sorted(ticker_twitter, key=lambda x: x['Date']):
+                        dt = datetime.strptime(row['Date'], '%Y-%m-%d')
+                        date = dt.strftime('%Y%m%d') # 2020-05-08
+                        info = f"{row['Followers']},{row['pct_change_day']},{row['pct_change_week']},{row['pct_change_month']}"
 
-            for row in sorted(ticker_twitter, key=lambda x: x['Date']):
-                dt = datetime.strptime(row['Date'], '%Y-%m-%d')
-                date = dt.strftime('%Y%m%d') # 2020-05-08
-                info = f"{row['Followers']},{row['pct_change_day']},{row['pct_change_week']},{row['pct_change_month']}"
-
-                lines.append(','.join([date, info]))
+                        lines.append(','.join([date, info]))
                 
-                with open(universe_file_path + date.replace("-", "") + '.csv', 'a') as ticker_file:
-                    sid = GenerateUSEquitySecurityIdentifier(ticker, dt)                
-                    ticker_file.write(f'{sid},{ticker},{info}\n')
+                        if self.canCreateUniverseFiles:
+                            with open( os.path.join(self.universeFolder, f'{date.replace("-", "")}.csv'), 'a') as fp:
+                                sid = SecurityIdentifier.GenerateEquity(ticker, Market.USA, True, mapFileProvider, dt)                
+                                fp.write(f'{sid},{ticker},{info}\n')
             
-            csv_lines = '\n'.join(lines)
-            with open(file_path + ticker.lower() + '.csv', 'w') as ticker_file:
-                ticker_file.write(csv_lines)
+                    with open(filename, 'w') as ticker_file:
+                        ticker_file.write('\n'.join(lines))
 
-            print(f'Finished processing {ticker}')
-            break
+                    print(f'Finished processing {ticker}')
+                    break                  
 
-        except Exception as e:
-            print(f'{e} - Failed to parse data for {ticker} - Retrying')
-            time.sleep(1)
-            i = i - 1
+                except Exception as e:
+                    print(f'{e} - Failed to parse data for {ticker} - Retrying')
+                    time.sleep(1)
+                    i = i - 1
+
+    def HttpRequester(self, url):       
+        base_url = 'https://api.quiverquant.com/beta'
+        headers = { 'accept': 'application/json', 'Authorization': 'Token ' + self.clientKey }
+        return requests.get(f'{base_url}/{url}', headers=headers).json()
+
+
+if __name__ == "__main__":
+    
+    dataFolder = Config.Get("data-folder", Globals.DataFolder)
+    canCreateUniverseFiles = os.path.isdir(os.path.join(dataFolder, 'equity','usa','map_files'))
+
+    destinationDirectory = os.path.join(Config.Get("temp-output-directory", "/temp-output-directory"), "alternative", f"{VendorName}")
+    instance = QuiverQuantTwitterFollowersDataDownloader(destinationDirectory, canCreateUniverseFiles);
+    instance.Run()
